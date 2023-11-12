99e5ecf3ef17e7dd27fa3ed9cfd1ccc5d567c9c6
godhyzzang <amksh0914@gmail.com>
2023-11-12 22:43:38 +0900

    실습 4 : 문자열 길이 계산

--- ex1109/main.c
+++ ex1109/main.c
@@ -1,17 +1,7 @@
 //6번 실습
 #include <stdio.h>
-
-int  main(void) {
-	FILE* fp = NULL;
-	char c;
-	fp = fopen_s(&fp, "sample.txt", "r");
-	//FILE** 과 FILE*은 다름
-
-	if (fp == NULL)
-		printf("파일을 못열음\n");
-	while ((c = fgetc(fp)) != EOF) //한글자 읽은게 마지막이 아니면
-		putchar(c); //한글자 출력 
-
-	fclose(fp);
-	return 0;
+#include <string.h>
+void main(void) {
+	char str[30] = "happy C programming";
+	printf("문자열 \"%s\"의 길이 : % i", str, strlen(str));
 }
