8ee491bf6326097a1daf9d7d9fce17a9405751ca
godhyzzang <amksh0914@gmail.com>
2023-11-12 22:36:18 +0900

    실습 5 : 입력 받은 단어를 파일로 출력

--- ex1109/main.c
+++ ex1109/main.c
@@ -1,14 +1,31 @@
+//5번 실습
+
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>     // for _countof
+//#include <stdlib.h>     // for _countof
 
 int main(void) {
-	
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+
+	FILE* fp;
+	char str[100];
+	//1. open file
+	fp = fopen_s(fp, "sample.txt", "w");
+	//2. write file
+
+	for (int i = 0; i < 3; i++) {
+
+		printf("input a world");
+		scanf_s("%c", str);//문자배열의 이름은 그 자체가 포인터.
+
+		//파일 내용 쓰기 
+		fprintf(fp, "%s\n", str);
+	}
 
 
-		strcpy_s(dst, _countof(dst),src); //strcpy_s instead of strcpy
 
-		printf("copied string :%s", dst);
+
+
+	//3. close file
+	fclose(fp);
+
 }
